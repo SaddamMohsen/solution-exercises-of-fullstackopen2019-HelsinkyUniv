ff23faaf51469015c221070d1faae7051f313e65
SaddamMohsen <sad.2000@outlook.com>
2020-01-02 15:45:34 +0300

    solution of 6.1 and 6.2 exercises

---
+++ part6/UnicafeRedux/src/reducer.js
@@ -0,0 +1,26 @@
+const initialState = {
+  good: 0,
+  ok: 0,
+  bad: 0
+}
+
+const unicafeReducer = (state = initialState, action) => {
+  //console.log(action)
+  switch (action.type) {
+    case 'GOOD':
+        //initialState={...initialState,good:initialState.good+1}
+         return {...state,good:state.good+1}
+    case 'OK':
+      //initialState={...initialState,ok:initialState.ok+1}
+         return {...state,ok:state.ok+1}
+    case 'BAD':
+      //initialState={...initialState,bad:initialState.bad+1}
+         return {...state,bad:state.bad+1}
+    case 'ZERO':
+        state=initialState
+      return state
+  }
+  return state
+}
+
+export default unicafeReducer
